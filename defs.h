36b90ed79be390de07d986722fc497eb1c5b04f4
heap-s <dqkushbro@gmail.com>
2024-05-20 12:52:58 -0700

    make state machine for lexical analysis of tokens, parsing next, not sure how to handle the return token/keyword in c

--- defs.h
+++ defs.h
@@ -4,7 +4,7 @@
 #include <ctype.h>
 
 enum {
-    Token_PLUS, Token_MINUS, Token_STAR, Token_INTLIT, Token_FSLASH, Token_SEMI, Token_BSLASH, Token_PERCENT, Token_LPAREN, Token_RPAREN, Token_RETURN, Token_INT, Token_LBRACE, Token_RBRACE    
+    Token_PLUS, Token_MINUS, Token_STAR, Token_INTLIT, Token_FSLASH, Token_SEMI, Token_BSLASH, Token_PERCENT, Token_LPAREN, Token_RPAREN, Token_RETURN, Token_LBRACE, Token_RBRACE    
 };
 
 struct {
--- lex.c
+++ lex.c
@@ -36,3 +36,74 @@ static int skipchr(void){
 
     return c;
 }
+
+static int scanint(int c){
+    int k, val = 0;
+
+    while ((k = chrpos("0123456789", c)) >= 0){
+        val = val * 10 +k;
+        c = next(); 
+    }
+    ungetc(c, Infile);
+
+    return val;
+}
+
+int scan (struct token *t){
+    int c;
+    
+    c = skip();
+
+    //state machine for lexing
+    switch (c){
+    case EOF:
+        return 0;
+    case '+':
+        t -> token = Token_PLUS;
+        break;
+    case '-':
+        t -> token = Token_MINUS;
+        break;
+    case '/':
+        t -> token = Token_FSLASH;
+        break;
+    case '*':
+        t -> token = Token_STAR;
+        break;
+    case ';':
+        t -> token = Token_SEMI;
+        break;
+    case '\\':
+        t -> token = Token_BSLASH;
+        break;
+    case '%':
+        t -> token = Token_PERCENT;
+        break;
+    case '(':
+        t -> token = Token_RPAREN;
+        break;
+    case ')':
+        t -> token = Token_LPAREN;
+        break;
+    case '{':
+        t -> token = Token_RBRACE;
+        break;
+    case '}':
+        t -> token = Token_LBRACE;
+        break;
+    case "return":
+        t -> token = Token_RETURN;
+        break;
+    default:
+        if (isdigit(c)){
+            t -> intvalue = scanint(c);
+            t -> token = T_INTLIT; 
+            break;
+        }
+    printf("Unrecognized character %c on line %d\n", c, Line);
+
+    exit(1);
+
+    }
+    return (1);
+}
